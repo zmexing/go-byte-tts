dd71c65cea1ed758aa22eeeb0170e9046ec1e4d7
libin <58850169@qq.com>
2024-05-30 09:17:07 +0800

    优化协程语音合成 (#4)
    * feat：协程支持语音合成
    
    * Fix: 修复Map引用造成的协程数据问题

---
+++ internal/params.go
@@ -0,0 +1,32 @@
+package internal
+
+import "errors"
+
+func CheckParams(params map[string]map[string]any) error {
+	if params["audio"] == nil {
+		params["audio"] = make(map[string]any)
+	}
+	if _, ok := params["audio"]["voice_type"]; !ok {
+		return errors.New("audio.voice_type cannot be empty")
+	}
+	if params["request"] == nil {
+		params["request"] = make(map[string]any)
+	}
+	_, ok := params["request"]["text"]
+	if !ok {
+		return errors.New("request.text cannot be empty")
+	}
+	return nil
+}
+
+func DeepCopyParams(original map[string]map[string]any) map[string]map[string]any {
+	copyData := make(map[string]map[string]any)
+	for k, v := range original {
+		innerCopy := make(map[string]any)
+		for innerK, innerV := range v {
+			innerCopy[innerK] = innerV
+		}
+		copyData[k] = innerCopy
+	}
+	return copyData
+}
--- tts.go
+++ tts.go
@@ -32,8 +32,10 @@ const (
 type GoTTSInter interface {
 	// TextToVoice 文本转语音
 	TextToVoice(params map[string]map[string]any) (*http.Response, func(), error)
+
 	// TextToVoiceDisk 文本转语音并写入磁盘
 	TextToVoiceDisk(params map[string]map[string]any, outFile *os.File) error
+
 	// TextToJoinVoiceDisk 文本转语音并写入磁盘
 	// 方法 [TextToVoiceDisk] 因为超过1024字节提示系统错误，所以建议使用 [TextToJoinVoiceDisk]
 	// 该方法会自动将文本按照 1024 字节将文本拆开，最后分片生成后合并成一个语音文件
@@ -42,6 +44,7 @@ type GoTTSInter interface {
 	// LongTextToVoiceCreate 长文本语音合成 任务创建
 	// 创建合成任务的频率限制为10 QPS，请勿一次性提交过多任务。
 	LongTextToVoiceCreate(params map[string]any) (*TtsAsyncRep, error)
+
 	// LongTextToVoiceId 长文本语音合成 任务查询
 	// 音频URL，有效期为1个小时，请及时下载
 	LongTextToVoiceId(id string) (*TtsAsyncQueryRep, error)
@@ -99,23 +102,6 @@ func WithEmotion() Option {
 	}
 }
 
-func (g *GoTTS) checkParams(params map[string]map[string]any) error {
-	if params["audio"] == nil {
-		params["audio"] = make(map[string]any)
-	}
-	if _, ok := params["audio"]["voice_type"]; !ok {
-		return errors.New("audio.voice_type cannot be empty")
-	}
-	if params["request"] == nil {
-		params["request"] = make(map[string]any)
-	}
-	_, ok := params["request"]["text"]
-	if !ok {
-		return errors.New("request.text cannot be empty")
-	}
-	return nil
-}
-
 // TextToVoiceDisk 文本转语音并写入磁盘
 func (g *GoTTS) TextToVoiceDisk(params map[string]map[string]any, outFile *os.File) error {
 	resp, funcClose, err := g.TextToVoice(params)
@@ -143,47 +129,12 @@ func (g *GoTTS) TextToVoiceDisk(params map[string]map[string]any, outFile *os.Fi
 	return internal.WriteBytesToDisk(audio, outFile)
 }
 
-func (g *GoTTS) TextToJoinVoiceDisk(params map[string]map[string]any, outFile *os.File) error {
-	if err := g.checkParams(params); err != nil {
-		return err
-	}
-	text, _ := params["request"]["text"]
-	textList := internal.SplitText(anyUtil.AnyToStr(text), 1024)
-
-	resAudio := []byte{}
-
-	for _, v := range textList {
-		params["request"]["text"] = v
-		resp, funcClose, err := g.TextToVoice(params)
-		defer funcClose()
-		if err != nil {
-			return err
-		}
-
-		respBody, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		rep := &Rep{}
-		err = json.Unmarshal(respBody, rep)
-		if err != nil {
-			return err
-		}
-
-		audio, err := base64.StdEncoding.DecodeString(rep.Data)
-		if err != nil {
-			return err
-		}
-
-		resAudio = append(resAudio, audio...)
-	}
-
-	return internal.WriteBytesToDisk(resAudio, outFile)
-}
-
 // TextToVoice 文本转语音
 func (g *GoTTS) TextToVoice(params map[string]map[string]any) (*http.Response, func(), error) {
+	if err := internal.CheckParams(params); err != nil {
+		return nil, func() {}, fmt.Errorf("invalid parameters: %w", err)
+	}
+
 	if params["app"] == nil {
 		params["app"] = make(map[string]any)
 	}
@@ -216,7 +167,7 @@ func (g *GoTTS) TextToVoice(params map[string]map[string]any) (*http.Response, f
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, funcClose, errors.New(resp.Status)
+		return nil, funcClose, fmt.Errorf("http response code failed: %w", errors.New(resp.Status))
 	}
 
 	if resp.ContentLength == 0 {
@@ -328,4 +279,75 @@ func (g *GoTTS) LongTextToVoiceId(id string) (*TtsAsyncQueryRep, error) {
 		return nil, fmt.Errorf("http response body Unmarshal error: %w", err)
 	}
 	return &result, nil
+}
+
+func (g *GoTTS) TextToJoinVoiceDisk(params map[string]map[string]any, outFile *os.File) error {
+	text, _ := params["request"]["text"]
+	textList := internal.SplitText(anyUtil.AnyToStr(text), 1024)
+
+	// 协程并行处理多个文本列表
+	chWork := make(chan ChanJoinVoice, len(textList))
+	defer close(chWork)
+	for i, v := range textList {
+		newMap := internal.DeepCopyParams(params)
+		newMap["request"]["text"] = v
+
+		go g.workTextToJoinVoiceDisk(newMap, i, chWork)
+	}
+
+	// 按照顺序拼接协程的数据
+	resMap := make(map[int][]byte, len(textList))
+	for i := 0; i < len(textList); i++ {
+		wordRes := <-chWork
+		if wordRes.Err != nil {
+			return wordRes.Err
+		}
+		resMap[wordRes.Index] = wordRes.Audio
+	}
+
+	// 按照顺序拼接结果
+	resAudio := []byte{}
+	for i, _ := range textList {
+		r, ok := resMap[i]
+		if !ok {
+			return errors.New("error in sequential splicing")
+		}
+		resAudio = append(resAudio, r...)
+	}
+	return internal.WriteBytesToDisk(resAudio, outFile)
+
+}
+
+func (g *GoTTS) workTextToJoinVoiceDisk(params map[string]map[string]any, idx int, ch chan ChanJoinVoice) {
+	params["request"]["reqid"] = uuid.NewString()
+
+	resp, funcClose, err := g.TextToVoice(params)
+	defer funcClose()
+	if err != nil {
+		ch <- ChanJoinVoice{Index: idx, Err: fmt.Errorf("TextToVoice error: %w", err)}
+		return
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		ch <- ChanJoinVoice{Index: idx, Err: fmt.Errorf("ReadAll error: %w", err)}
+		return
+	}
+
+	var rep Rep
+	if err := json.Unmarshal(respBody, &rep); err != nil {
+		ch <- ChanJoinVoice{Index: idx, Err: fmt.Errorf("JSON unmarshal error: %w", err)}
+		return
+	}
+
+	audio, err := base64.StdEncoding.DecodeString(rep.Data)
+	if err != nil {
+		ch <- ChanJoinVoice{Index: idx, Err: fmt.Errorf("base64 decode error: %w", err)}
+		return
+	}
+
+	ch <- ChanJoinVoice{
+		Index: idx,
+		Audio: audio,
+	}
 }
